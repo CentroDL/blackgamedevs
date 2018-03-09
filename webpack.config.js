86bdcaf8b573b4c35c1d9c38fe40bc8c3185a7f7
Dennis Liaw <dennis.liaw@gmail.com>
2018-03-08 23:18:53 -0500

    Adds react router 4

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,12 @@ module.exports = {
         loader: ExtractTextPlugin.extract({
           use: 'css-loader'
         })
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   },
